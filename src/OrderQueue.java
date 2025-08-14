bfb4176410305c140be1dd86d6b2dea42bb73fef
lamabeta <148013077+lamabeta@users.noreply.github.com>
2025-08-14 12:22:34 +0300

    Update OrderQueue.java

--- src/OrderQueue.java
+++ src/OrderQueue.java
@@ -1,3 +1,17 @@
 public class OrderQueue {
 
+  public static void main(String[] args) {
+    OrderQueue tracker = new OrderQueue();
+    tracker.addOrder(new Order(["Laptop", "Monitor"], "Fahad", "A001"));
+    tracker.addOrder(new Order(["Earrings", "Bracelets", "Necklaces"], "Noura", "A002"));
+    tracker.addOrder(new Order(["UNO", "Monopoly"], "Sara", "A003"));
+
+    tracker.FirstOrder();
+    tracker.TotalOrders();
+
+    tracker.processOrder();
+
+    tracker.FirstOrder();
+    tracker.TotalOrders();
+    }
 }
